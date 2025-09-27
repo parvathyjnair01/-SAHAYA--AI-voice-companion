a5f99515bb2fedcc298bdb708d05922a4cb8e2dc
Nayana V Nair <nayananair2006@gmail.com>
2025-09-27 09:10:54 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById("profileForm").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("✅ Profile created successfully! (In real app, this would save da = "./main.html";
+  window.location.href = "./main.html";
 });
+
 
